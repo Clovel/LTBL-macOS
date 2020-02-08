718c3759e72fbc201877547fcdbe3afd64c77a17
Clovis Durand <cd.clovel19@gmail.com>
2020-02-08 18:44:17 +0100

    Added LTBL class source code

---
+++ LTBL-macOS/LTBL.swift
@@ -0,0 +1,66 @@
+import Foundation
+
+struct LTBL_swift_hello_world {
+    var text = "Hello, World!"
+}
+
+/**
+ * @brief Different states the LTBL device can be in.
+ */
+enum LTBLState {
+    case LTBL_UNKNOWN_STATE
+    case LTBL_ON
+    case LTBL_OFF
+}
+
+/**
+ * @class This class represents a LTBL device.
+ */
+class LTBLDevice {
+    var state: LTBLState = LTBLState.LTBL_UNKNOWN_STATE;
+    var IPAddr: String?;
+
+    func getHomePage() -> Void {
+        let url = URL(string: "http://" + IPAddr!)!
+        let urlSession: URLSession = URLSession.shared;
+        let urlSessionTask: URLSessionDataTask = urlSession.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+                print("[DEBUG] <LTBLDevice::getHomePage> Data = ", data as Any)
+                print("[DEBUG] <LTBLDevice::getHomePage> Response = ", response as Any)
+                print("[DEBUG] <LTBLDevice::getHomePage> Error = ", error as Any)
+            });
+
+        urlSessionTask.resume();
+    }
+
+    func toggle() {
+        //
+    }
+
+    func switchState(pState: LTBLState) {
+        //
+    }
+
+    func turnOn() {
+        let url = URL(string: "http://" + IPAddr! + "/on")!
+        let urlSession: URLSession = URLSession.shared;
+        let urlSessionTask: URLSessionDataTask = urlSession.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+                print("[DEBUG] <LTBLDevice::getHomePage> Data = ", data as Any)
+                print("[DEBUG] <LTBLDevice::getHomePage> Response = ", response as Any)
+                print("[DEBUG] <LTBLDevice::getHomePage> Error = ", error as Any)
+            });
+
+        urlSessionTask.resume();
+    }
+
+    func turnOff() {
+        let url = URL(string: "http://" + IPAddr! + "/off")!
+        let urlSession: URLSession = URLSession.shared;
+        let urlSessionTask: URLSessionDataTask = urlSession.dataTask(with: url, completionHandler: { (data: Data?, response: URLResponse?, error: Error?) in
+                print("[DEBUG] <LTBLDevice::getHomePage> Data = ", data as Any)
+                print("[DEBUG] <LTBLDevice::getHomePage> Response = ", response as Any)
+                print("[DEBUG] <LTBLDevice::getHomePage> Error = ", error as Any)
+            });
+
+        urlSessionTask.resume();
+    }
+}
