0470434753e61d6eb8a025f65c2ba288c018ef24
Clovis Durand <cd.clovel19@gmail.com>
2020-02-10 21:34:52 +0100

    Initial commit

---
+++ LTBL-macOS/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  LTBL-macOS
+//
+//  Created by Clovis Durand on 08/02/2020.
+//  Copyright © 2020 Clovis Durand. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
